94caef104ab663f36ed0ada42fdb90d8823ef233
Ivan Alvarez <alvarez.iv64@gmail.com>
2022-01-20 19:05:03 -0300

    clase 3

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import './navbar.css'
+
+const NavBar = () => {
+    return (
+
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Galeria</li>
+                <li>Info</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+
+    );
+};
+
+export default NavBar;
